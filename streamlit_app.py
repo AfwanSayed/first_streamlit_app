a48c64cda51d76febdcd44f089757e8fa66302a5
AfwanSayed <127505780+AfwanSayed@users.noreply.github.com>
2023-03-10 18:33:50 +0530

    BreakFast Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
-streamlit.title('hello Worlds')
+streamlit.title('My Parents new Healty Dinner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oat Meal')
+streamlit.text('Spinich')
+streamlit.text('Egg')
